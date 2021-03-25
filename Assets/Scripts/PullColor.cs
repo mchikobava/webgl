31d572d38e3c1697635e345c6a6295c66b067b1a
Margarita Chikobava <margarita.chikobava@dfki.de>
2021-03-25 20:23:59 +0100

    initial

---
+++ Assets/Scripts/PullColor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Networking;
+
+
+public class PullColor : MonoBehaviour
+{
+    public Material green;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Onclick()
+    {
+        Debug.Log("onClick");
+        StartCoroutine(ColorOnRequest("https://669a3785-9e91-4232-965f-118bc9bb0f6b.mock.pstmn.io/color"));
+
+
+    }
+
+    // changes color to the color pulled from the server
+    private IEnumerator ColorOnRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            GameObject cube = GameObject.Find("Cube");
+            if (webRequest.downloadHandler.text == "green")
+            {
+                cube.GetComponent<MeshRenderer>().material = green;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour
+{
+    NetworkServerSimple server = null;
+    //UnityWebRequest webRequest
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Init()
+    {
+        NetworkTransport.Init();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
